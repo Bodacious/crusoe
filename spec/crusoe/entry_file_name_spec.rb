c4a7d2d84bbc1c472add3123e54234b76bf85120
Bodacious <gavin@gavinmorrice.com>
2023-10-29 16:13:19 +0000

    Add spec for entry file name

---
+++ spec/crusoe/entry_file_name_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+require "crusoe/entry_file_name"
+
+RSpec.describe Crusoe::EntryFileName do
+  it "#to_file_path returns the correct file path" do
+    entry = double("Entry", year: 2023, month: 10, day: 28)
+    entries_directory = "/path/to/entries"
+    entry_file_name = described_class.new(entry, entries_directory)
+    expected_file_path = "/path/to/entries/2023-10/2023-10-28.md"
+
+    expect(entry_file_name.to_file_path).to eq(expected_file_path)
+  end
+
+  it "#to_s method is aliased to to_file_path" do
+    entry = double("Entry", year: 2023, month: 10, day: 28)
+    entries_directory = "/path/to/entries"
+    entry_file_name = described_class.new(entry, entries_directory)
+
+    expect(entry_file_name.to_s).to eq(entry_file_name.to_file_path)
+  end
+end
